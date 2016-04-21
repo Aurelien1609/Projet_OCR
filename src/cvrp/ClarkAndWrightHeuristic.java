c7e65a55c3c3355da7625589afed997eae289390
Aurelien1609 <marieaurelien1609@gmail.com>
2016-04-21 23:43:43 +0200

    MAJ

--- src/cvrp/ClarkAndWrightHeuristic.java
+++ src/cvrp/ClarkAndWrightHeuristic.java
@@ -9,27 +9,43 @@ import util.CustomList;
 public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 
 	double[][] matrix = null;
-	int capacity = 0;
+	int capacity;
 	int[] demands = null;
-	int nbCustomer = 0;
+	int nbCustomer;
 	
 	@Override
 	public double computeSolution(VRPinstance instance) {
 		// TODO Auto-generated method stub
 		
-		matrix = instance.getMatrix();
-		capacity = instance.getCapacity();
-		demands = instance.getDemands();
-		nbCustomer = instance.getN();
+//		matrix = instance.getMatrix();
+//		capacity = instance.getCapacity();
+//		demands = instance.getDemands();
+//		nbCustomer = instance.getN();
+		
+		matrix = new double[4][4];
+		matrix[1][0] = matrix[0][1] = 2;
+		matrix[2][0] = matrix[0][2] = 3;
+		matrix[3][0] = matrix[0][3] = 2;
+		matrix[1][2] = matrix[2][1] = 1;
+		matrix[2][3] = matrix[3][2] = 2;
+		matrix[1][3] = matrix[3][1] = 3;		
+		
+		capacity = 3; 
+		demands = new int[4];
+		demands[0] = 0;
+		demands[1] = demands[2] = demands[3] = 1;
+		nbCustomer = 4;
+
 		
 		ArrayList<CustomList<Integer>> L = new ArrayList<CustomList<Integer>>();
 		CustomList<Integer> tmp;
 		
 		// initial solution
-		for (int i = 1; i < nbCustomer; i++) {
+		for (int i = 2; i <= nbCustomer; i++) {
 			tmp = new CustomList<Integer>();
 			tmp.add(i);
 			L.add(tmp);
+//			System.out.println(tmp);
 		}
 		
 		Boolean positiveSavings = true;
@@ -45,6 +61,7 @@ public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 			tmpSavings = 0;
 			posIBestSavings = 0;
 			posJBestSavings = 0;
+			sumDemand = 0;
 
 //			for (CustomList<Integer> tour : L) {
 //				System.out.println(tour);
@@ -56,14 +73,15 @@ public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 				for (int j = i + 1; j < L.size(); j++) {					
 					
 					tmpSavings = computeSaving(L.get(i), L.get(j));
-					System.out.println("-----------");
+//					System.out.println("-----------");
 					sumDemand = sumDemand(L.get(i)) + sumDemand(L.get(j));
 					
-					//System.out.println(L.get(i) + " et " + L.get(j));
-					//if (sumDemand <= capacity) System.out.println("capacité OK : " + sumDemand + " pour " + capacity);
+//					System.out.println(L.get(i) + " et " + L.get(j));
+//					if (sumDemand <= capacity) System.out.println("capacité OK : " + sumDemand + " pour " + capacity);
+//					System.out.println(tmpSavings);
 
 					if (tmpSavings > bestSavings && sumDemand <= capacity) {
-						//System.out.println("meilleur solution de fusion : " + tmpSavings + "contre " + bestSavings);
+//						System.out.println("meilleur solution de fusion : " + tmpSavings + "contre " + bestSavings);
 						posIBestSavings = i;
 						posJBestSavings = j;
 						bestSavings = tmpSavings;
@@ -72,7 +90,10 @@ public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 			}
 						
 			if (bestSavings > 0) {
-				//System.out.println("Finalement, fusion de : " + L.get(posIBestSavings) + " et " + L.get(posJBestSavings));
+				System.out.println("Meilleur fusion : " + L.get(posIBestSavings) + " et " + L.get(posJBestSavings) + " pour " + bestSavings);
+				System.out.println("Demande de " + L.get(posIBestSavings) + " : " + sumDemand(L.get(posIBestSavings)));
+				System.out.println("Demande de " + L.get(posJBestSavings) + " : " + sumDemand(L.get(posJBestSavings)));
+				System.out.println("Totale demande : " + (sumDemand(L.get(posIBestSavings)) + sumDemand(L.get(posJBestSavings))) + " Capacité : " + capacity);
 				L.get(posIBestSavings).append(L.get(posJBestSavings));
 				L.remove(posJBestSavings);
 			}
@@ -123,23 +144,26 @@ public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 	
 	public double costTour(CustomList<Integer> tour) {		
 //		System.out.println(tour);
-//		System.out.println("cout de 0-" + tour.getFirst() + " = " + matrix[0][tour.getFirst()]);
+//		System.out.println("cout de 1-" + tour.getFirst() + " = " + matrix[0][tour.getFirst() - 1]);
+//		System.out.println("cout de " + tour.getLast() + "-1 = " + matrix[tour.getLast() - 1][0]);
+
 		
-		double cost = matrix[0][tour.getFirst()] + matrix[tour.getLast()][0]; 
+		double cost = matrix[0][tour.getFirst() - 1] + matrix[tour.getLast() - 1][0]; 
+//		System.out.println("cout : " + cost);
 		
 		Iterator<Integer> ite = tour.iterator();
-		int node1 = ite.next();
+		int node1 = ite.next() - 1;
 		int node2 = 0;
 		
 		try {
 			while (ite.hasNext()) {
-				node2 = ite.next();
+				node2 = ite.next() - 1;
 //				System.out.println("cout de " + node1 + "-" + node2 + " = " + matrix[node1][node2]);
 				cost += matrix[node1][node2];
 				node1 = node2;
 			}
 			
-			node2 = ite.next();
+			node2 = ite.next() - 1;
 //			System.out.println("cout de " + node1 + "-" + node2 + " = " + matrix[node1][node2]);
 			cost += matrix[node1][node2];
 		} 
@@ -153,19 +177,19 @@ public class ClarkAndWrightHeuristic implements HeuristicCVRP {
 	
 	public double sumDemand(CustomList<Integer> tour) {
 		
-		System.out.println(tour);
+		//System.out.println(tour);
 		double sumDemand = 0;
 		Iterator<Integer> ite = tour.iterator();
 		int node = 0;
 		while (ite.hasNext()) {
-			node = ite.next();
-			System.out.println(node + " pour une valeur de " + demands[node]);
+			node = ite.next() - 1;
+			//System.out.println(node + " pour une valeur de " + demands[node]);
 			sumDemand += demands[node];
 		}
 		
-		node = ite.next();
+		node = ite.next() - 1;
 		sumDemand += demands[node];
-		System.out.println(node + " pour une valeur de " + demands[node]);
+		//System.out.println(node + " pour une valeur de " + demands[node]);
 
 			
 		return sumDemand;
